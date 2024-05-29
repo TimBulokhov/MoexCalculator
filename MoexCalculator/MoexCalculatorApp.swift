95a9f8b7f9e9f5d2e544137c1afab4107e459993
Tim Bulokhov <timofeybrant@mail.ru>
2024-05-29 19:52:46 +0200

    Initial Commit

---
+++ MoexCalculator/MoexCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoexCalculatorApp.swift
+//  MoexCalculator
+//
+//  Created by Timofey Bulokhov on 29.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoexCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
